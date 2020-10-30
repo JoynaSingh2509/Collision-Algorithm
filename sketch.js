c54ea62f7fae770d329c1c3fd97fb12bb40c36fe
JoynaSingh2509 <73386020+JoynaSingh2509@users.noreply.github.com>
2020-10-30 16:00:38 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,24 @@
+var f,m;
+
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  f=createSprite(400, 200, 80, 50);
+  m=createSprite(200, 200, 50, 80);
 }
 
 function draw() {
   background(255,255,255);  
+
+  m.x=mouseX;
+  m.y=mouseY;
+
+  if(m.x-f.x<m.width/2+f.width/2&&f.y-m.y<m.height/2+f.height/2&&m.y-f.y<m.height/2+f.height/2&&f.x-m.x<m.width/2+f.width/2){
+    f.shapeColor="red";
+    m.shapeColor="red";
+  }
+  else{
+    f.shapeColor="green";
+    m.shapeColor="green";
+  }
   drawSprites();
 }
